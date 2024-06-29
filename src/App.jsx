911cbd17b7d5ee90ccabf6860c98f3977c65c86f
TABASSUM KHANUM <55125208+tabassum2507@users.noreply.github.com>
2024-06-29 23:50:13 +0530

    project started

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <p className='text-3xl'>Hello</p>
+    </>
+  )
+}
+
+export default App
